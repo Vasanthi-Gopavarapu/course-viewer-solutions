4a64ba3b94da14ffc23708df55774afee57059fc
Vasanthi-Gopavarapu <vasanthi.gopavarapu0205@gmail.com>
2020-07-10 19:15:19 +0530

    commit 2 with EX-03 from Course-viewer

--- course-viewer/src/Courses.js
+++ course-viewer/src/Courses.js
@@ -2,15 +2,87 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Courses() {
- 
+class Courses extends React.Component {
+    constructor(props) {
+        super(props);
+        if(this.state === undefined || this.state === null)
+        {
+             this.state = {
+                value: '',
+                items: []
+             };
+        }
+             console.log("constructor", this.state);
+        
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async handleChange(e) {
+        console.log("handlechange initial state", this.state);
+         await this.setState({value: e.target.value});
+        console.log(this.state);
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        console.log("before", this.state);
+        
+        await this.setState(state => {
+             const items = state.items.concat(state.value);
+
+             return {
+                 items,
+                 value:''
+             }
+        });
+        console.log("after", this.state);
+    }
+
+ render(){
+     console.log("render");
+     //const items = this.state.items;
       return (
         <div className="container p-5 my-3 bg-light">
           <h1>Courses</h1>
-        </div>
-        
+          <Addedcourses items={this.state.items}/>
+          <h2>Add Courses</h2>
+          <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                <label>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Save" />
+              </div>
+           </form>
+     </div> 
       );
+ }
+      
 }
+
+
+function Addedcourses(props) {
+    
+    const items = props.items
+    if(items.length > 0)
+    {
+    const listItems = items.map((item, index) => 
+    <li key={index}> {item} </li>
+    );
+    return (
+        <ul>
+            {listItems}
+        </ul>
+        );
+
+    }
+
+    return null;
+    
+}
+
 
 
 export default Courses;
